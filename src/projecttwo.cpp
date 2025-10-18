878dad0c50dab6afc5c7f595a8be5361450281f2
Owen G <78248420+hiiraeth@users.noreply.github.com>
2025-10-17 20:00:19 -0400

    documentation

--- src/projecttwo.cpp
+++ src/projecttwo.cpp
@@ -1,3 +1,5 @@
+// --- Includes --- ///
+
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -7,12 +9,18 @@
 #include <limits>
 #include <algorithm>
 
+// --- Object set up --- ///
+
+/**
+ * @brief Constructs a course object
+ */
 struct Course
 {
     std::string courseID;
     std::string courseTitle;
     std::vector<std::string> prerequisites;
 
+    // ctors
     Course()
     {
         courseID = "0";
@@ -27,6 +35,9 @@ struct Course
     }
 };
 
+/**
+ * @brief Constructs a hashtable object
+ */
 class HashTable 
 {
 
@@ -63,30 +74,43 @@ private:
 
     std::vector<Node> nodes;
 
-
 public:
+    // ctors and dtor
     HashTable();
     HashTable(unsigned int sizeOfTable);
     ~HashTable();
 
+    // methods
     void InsertCourse(Course course);
     void PrintAllCourses();
     void Search(std::string id);
 };
 
+// --- Method definitions --- ///
+
+/**
+ * @brief HashTable default ctor
+ */
 HashTable::HashTable()
 {
     nodes.resize(tableSize);
 }
 
+/**
+ * @brief Hashtable ctor with table size
+ */ 
 HashTable::HashTable(unsigned int sizeOfTable)
 {
     tableSize = sizeOfTable;
     nodes.resize(tableSize);
 }
 
+/**
+ * @brief Hashtable dtor
+ */
 HashTable::~HashTable()
 {
+    // deletes all elements from hashtable
     for (auto& head : nodes)
     {
         Node* current = head.next;
@@ -103,11 +127,32 @@ HashTable::~HashTable()
     }
 }
 
+/**
+ * @brief Hashing function 
+ * 
+ * Function will take in a key var (course's courseID),
+ * and return a key for the hashtable by moding the key
+ * by hashtable size
+ * 
+ * @param key   variable to hash
+ * 
+ * @return      returns hashed key
+ */
 int HashTable::hash(int key)
 {
     return key % tableSize;
 }
 
+/**
+ * @brief Insert course to hashtable
+ * 
+ * Function will first hash the courseID and implement
+ * the course into the table using said key. Chains course
+ * into bucket if the key is already in use by another
+ * course.
+ * 
+ * @param course    course to insert
+ */
 void HashTable::InsertCourse(Course course)
 {
     std::string courseIDnumber = course.courseID.substr(4, 3);
@@ -117,6 +162,7 @@ void HashTable::InsertCourse(Course course)
 
     Node& oldNode = nodes.at(bucketKey);
 
+    // checks if node is in use. if not, assign data to node
     if (oldNode.key == UINT_MAX)
     {
         oldNode.key = bucketKey;
@@ -124,7 +170,7 @@ void HashTable::InsertCourse(Course course)
         oldNode.next = nullptr;
         return;
     }
-    else
+    else // otherwise find next empty node
     {
         Node* currentNode = &oldNode;
 
@@ -138,48 +184,52 @@ void HashTable::InsertCourse(Course course)
     }
 }
 
+/**
+ * @brief Print all courses in hashtable, sorted
+ * 
+ * Sorts all elements in hashtable based on their
+ * courseID, and will then output them in order
+ */
 void HashTable::PrintAllCourses()
 {
     std::vector<Course> allCourses;
 
+    // inputs all courses into a vector
     for (auto& elem : nodes)
     {
         allCourses.push_back(elem.course);
     }
 
+    // lambda function to sort all elements
+    // variation of the following: https://stackoverflow.com/a/60753664
     std::sort(allCourses.begin(), allCourses.end(), [](auto a, auto b)
     {
         return a.courseID < b.courseID;
     });
 
+    // outputs each sorted elements, ignoring default cases
     for (auto& elem : allCourses)
     {
-        if (elem.courseID == "0" || elem.courseTitle == "Title")
+        if (elem.courseID == "0" || elem.courseTitle == "Title" || elem.courseID == "courseID")
             continue;
-            
+
         std::cout << elem.courseID << ", " <<
             elem.courseTitle << "\n";
     }
-    
-    // for (int i = 0; i < nodes.size(); i++)
-    // {
-    //     Node& head = nodes.at(i);
-    //     if (head.key == UINT_MAX)
-    //         continue;
-
-    //     Node* current = &head;
-    //     while (current != nullptr)
-    //     {
-    //         std::cout << current->course.courseID << ", " <<
-    //             current->course.courseTitle << "\n";
-            
-    //         current = current->next;
-    //     }
-    // }
 
     std::cout << "\n";
 }
 
+/**
+ * @brief Search for course
+ * 
+ * Searches for course in hashtable using the
+ * courseID. Hashes the ID to find its key, and
+ * then looks up the course at the key's
+ * corresponding node and outputs its data.
+ * 
+ * @param id    id used to search
+ */
 void HashTable::Search(std::string id)
 {
     int key = hash(atoi(id.substr(4,3).c_str()));
@@ -190,6 +240,7 @@ void HashTable::Search(std::string id)
     std::cout << searchNode->course.courseID << ", " << searchNode->course.courseTitle << "\n";
     std::cout << "Prerequisites: ";
 
+    // checking for prereqs
     if (!(prereqs.empty()))
     {
         for (std::string& elem : prereqs)
@@ -208,6 +259,18 @@ void HashTable::Search(std::string id)
     std::cout << "\n\n";
 }
 
+// --- Function definitions --- //
+
+/**
+ * @brief Trims courseID
+ * 
+ * Removes trailing and leading whitespace in the
+ * courseID, specifically spaces and tabs from the
+ * left and spaces, tabs, and '\r' from the right.
+ * Will then trim the courseID and return it by ref.
+ * 
+ * @return returns trimmed courseID by reference
+ */
 void trim(std::string& item)
 {
     int start = 0;
@@ -228,7 +291,16 @@ void trim(std::string& item)
 }
 
 /**
- * Load the CSV file into the hashtable
+ * @brief Load CSV file into hashtable
+ * 
+ * Traverses through the CSV file and adds data
+ * from each line into temporary row. It first reads
+ * through the file and saves each courseID for later
+ * validation. It then reads through a second time and
+ * cuts the data into parts using commas as delimiters.
+ * It saves the information in id and name, and validates
+ * the prerequisites by checking to make sure the course
+ * exists already. 
  * 
  * @param csvPath           path of CSV file to read from
  * @param courses           hashtable of courses
@@ -249,9 +321,7 @@ void loadFileIntoHashtable(std::string csvPath, HashTable* courses, std::vector<
         std::exit(EXIT_FAILURE);
     }
 
-    std::string headerLine;
-    std::getline(inFile, headerLine);
-
+    // reads in all courseids, saves for later validation
     while (inFile.good())
     {
         std::getline(inFile, line, ',');
@@ -261,17 +331,18 @@ void loadFileIntoHashtable(std::string csvPath, HashTable* courses, std::vector<
         else
             courseids.push_back(line);
 
+        // only reads the first column, aka where the course id is
         inFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
+    // resets inFile for second read
     inFile.clear();
     inFile.seekg(0);
 
-    std::getline(inFile, headerLine);
-
-    int rowCount = 1;
+    int rowCount = 1; // for error checking
     while (inFile.good())
     {
+        // reads through each line, resets temp vars each iter
         row.clear();
         prereqs.clear();
 
@@ -289,6 +360,7 @@ void loadFileIntoHashtable(std::string csvPath, HashTable* courses, std::vector<
         }
 
         rowCount++;
+
         if (row.size() < 2)
         {
             std::cerr << "ERROR: Row needs at least 2 values. Row " << rowCount << " not read!" << std::endl;
@@ -298,6 +370,7 @@ void loadFileIntoHashtable(std::string csvPath, HashTable* courses, std::vector<
         std::string id = row[0];
         std::string name = row[1];
 
+        // prerequisite validation
         if (row.size() > 2)
         {
             for (int i = 2; i < row.size(); i++)
@@ -312,6 +385,7 @@ void loadFileIntoHashtable(std::string csvPath, HashTable* courses, std::vector<
             }
         }
 
+        // add course to hashtable
         Course newCourse = Course(id, name, prereqs);
 
         courses->InsertCourse(newCourse);
@@ -320,6 +394,9 @@ void loadFileIntoHashtable(std::string csvPath, HashTable* courses, std::vector<
     inFile.close();
 }
 
+/**
+ * @brief Shows menu to user
+ */
 void showMenu()
 {
     std::cout << "|--------------------------\n";
@@ -331,6 +408,21 @@ void showMenu()
     std::cout << "|- Enter choice: ";
 }
 
+/**
+ * @brief Validates courseID for searching
+ * 
+ * Function first trims users input, and then
+ * checks the input against a vector of all
+ * available courseIDs. Returns true if user's
+ * search is in the vector, returns false if
+ * search is NOT in the vector. Will then search
+ * and output the course info using Search().
+ * 
+ * @param courseids all course ids
+ * @param choice    user's search input
+ * 
+ * @return true if valid course, false if invalid course
+ */
 bool validateID(std::vector<std::string>& courseids, std::string choice)
 {
     trim(choice);
@@ -346,7 +438,22 @@ bool validateID(std::vector<std::string>& courseids, std::string choice)
     return true;
 }
 
-int menuChoice(int choice, HashTable& courses, bool& load, std::vector<std::string>& courseids)
+/**
+ * @brief Main logic loop
+ * 
+ * Validates user input for the choice of menu
+ * item. Then proceeds with the respective functions
+ * based on user input, with input validation at
+ * each case.
+ * 
+ * @param choice        user input, menu choice
+ * @param courses       hashtable of all courses
+ * @param load          check if courses have already been loaded
+ * @param courseids     vector of all courseids for searching
+ * 
+ * @return returns user choice for main while loop
+ */
+int menuChoice(int choice, std::string& csvPath, HashTable& courses, bool& load, std::vector<std::string>& courseids)
 {
     const int MIN = 1;
     const int MAX = 3;
@@ -373,7 +480,7 @@ int menuChoice(int choice, HashTable& courses, bool& load, std::vector<std::stri
                 break;
             }
 
-            loadFileIntoHashtable("ABCU_Courses.csv", &courses, courseids);
+            loadFileIntoHashtable(csvPath, &courses, courseids);
             std::cout << "File loaded successfully.\n\n";
             load = true;
 
@@ -418,17 +525,41 @@ int menuChoice(int choice, HashTable& courses, bool& load, std::vector<std::stri
     return choice;
 }
 
-int main()
+/**
+ * @brief Processes command line arguments
+ * 
+ * @param argc      number of args passed
+ * @param argv      array of string args
+ * @param csvPath   path to CSV file
+ */
+void processCLIargs(int argc, char* argv[], std::string& csvPath)
+{
+    switch (argc) {
+        case 2:
+            csvPath = argv[1];
+            break;
+        default:
+            csvPath = "CS 300 ABCU_Advising_Program_Input.csv";
+            break;
+    }
+}
+
+// --- Main loop --- //
+
+int main(int argc, char* argv[])
 {
     int choice = 0;
+    std::string csvPath;
     std::vector<std::string> courseids;
     HashTable courses;
     bool isLoaded = false;
 
+    processCLIargs(argc, argv, csvPath);
+
     while (choice != 9)
     {
         showMenu();
-        choice = menuChoice(choice, courses, isLoaded, courseids);
+        choice = menuChoice(choice, csvPath, courses, isLoaded, courseids);
     }
 
     return 0;
